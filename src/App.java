02a04c3fc12b7dbf632f11e529897f5e4a85ac38
ManuelHMR <manuelhmrjf@gmail.com>
2023-01-10 14:49:51 -0300

    feat: exercise

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero para determinar o tamanho da piramide: ");
+        int num = scan.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        scan.close();
     }
 }
